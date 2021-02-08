2ddeef665cfbd3b05853cdc12a5edc616f47320e
Guvanch <mr.yazgulyyew@outlook.com>
2021-02-08 14:04:04 +0300

    Proje dosyası ekle.

---
+++ RenklerinDili/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RenklerinDili
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text=="Guga"&& textBox2.Text == "123")
+            {
+                Form2 renkfotrm = new Form2();
+                renkfotrm.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Giriş Hatalı!!");
+                textBox1.Clear();
+                textBox2.Clear();
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Form2 renk = new Form2();
+            renk.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ RenklerinDili/Form2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RenklerinDili
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox2.Text == "Mavi")
+            {
+                label4.Text = textBox1.Text + " " + comboBox2.Text + "rengi=" + "Mutlulugun ve pozitifligin rengidir.";
+                button2.BackColor = Color.Blue;
+            }
+            if (comboBox2.Text == "Sarı")
+            {
+                label4.Text = textBox1.Text + " " + comboBox2.Text + "rengi=" + "iyimserligin rengidir:";
+                button2.BackColor = Color.Yellow;
+            }
+            if (comboBox2.Text == "Kırmızı")
+            {
+                label4.Text = textBox1.Text + " " + comboBox2.Text + "rengi=" + "Şehvet,Arzu rengidir!";
+                button2.BackColor = Color.Red;
+            }
+            if (comboBox2.Text == "Siyah")
+            {
+                label4.Text = textBox1.Text + " " + comboBox2.Text + "rengi=" + "Kötümserligin rengi";
+                button2.BackColor = Color.Black;
+            }
+        }
+    }
+}
